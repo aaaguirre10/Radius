25621c5ad1d61c966feaf0a86d2466aed02be182
Antonio <aaaguirre10@miners.utep.edu>
2022-10-31 07:06:25 -0600

    Started project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './components/Home/Home'
+import Login from './components/Login/Login'
+import "./styles.css"
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home/>}></Route>
+        <Route exact path="/login" element={<Login/>}></Route>
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+  return (
+    <div className='Home'>
+        <div className='home-brand'>
+            <h1>RADIUS</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Home
