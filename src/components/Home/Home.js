5a074259ac69429de2e871421ba8fbf72d07a279
Antonio <aaaguirre10@miners.utep.edu>
2022-11-01 00:37:23 -0600

    update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,10 +3,9 @@ import './Home.css'
 
 function Home() {
   return (
-    <div className='Home'>
-        <div className='home-brand'>
-            <h1>RADIUS</h1>
-        </div>
+    <div className='home-brand'>
+        <h1>RADIUS</h1>
+        <p>Are we in reach?</p>
     </div>
   )
 }
