6dd61b1dba84aefbdedd1162d8fbddfb53fdb3ad
alan <agamez8@miners.utep.edu>
2022-11-05 10:07:17 -0600

    back end update

--- nearby/nearby.py
+++ nearby/nearby.py
@@ -1,8 +1,22 @@
 import bluetooth
 
+import sockets
+
 def get_nearby():
     nearby_devices = bluetooth.discover_devices(lookup_names = True) # find all nearby devices
-    print("found {} devices.".format(len(nearby_devices)))
+    #print("found {} devices.".format(len(nearby_devices)))
 
     for addr, name in nearby_devices:
-        print(" {} - {}".format(addr, name)) # display device names
+        print("name=", name) # display device names
+        print("addr=", addr) # display address names
+    
+    return nearby_devices
+
+def get_port():
+    s = sockets.socket(sockets.AF_INET, sockets.SOCK_STREAM)
+    s.bind(("",0))
+    s.listen(1)
+    port = s.getsockname()[1]
+    s.close()
+
+    return port
