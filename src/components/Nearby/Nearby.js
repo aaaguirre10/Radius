179fd2f709b96254ee01b0ddc3da0fa053e85ddc
daviddguez8 <dedominguez2@miners.utep.edu>
2022-11-22 22:55:21 -0600

    connect add friend button to blockchain

--- src/components/Nearby/Nearby.js
+++ src/components/Nearby/Nearby.js
@@ -1,32 +1,25 @@
 import React, { Component } from "react";
 import './Nearby.css'
 import Navbar from '../Navbar/Navbar'
+import { sha256 } from "js-sha256";
+import { fetchNearby, sendFriendRequest } from '../../backend/nearby';
 
 class Nearby extends Component {
-  state = {
-    data: [],
-    per: 4,
-    page: 1,
-    total_pages: null
-  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      nearbyUsers: [],
+      per: 4,
+      page: 1,
+      total_pages: null
+    }
+  }
 
   uppercase = word => {
     return word.charAt(0).toUpperCase() + word.slice(1);
   };
 
-  loadData = () => {
-    const { per, page, data } = this.state;
-    const endpoint = `https://randomuser.me/api/?nat=us&results=${per}&page=${page}`;
-    fetch(endpoint)
-      .then(response => response.json())
-      .then(json => {
-        this.setState({
-          data: [...data, ...json.results],
-          scrolling: false,
-          total_pages: json.info.results
-        });
-      });
-  };
 
   loadMore = () => {
     this.setState(
@@ -34,12 +27,34 @@ class Nearby extends Component {
         page: prevState.page + 1,
         scrolling: true
       }),
-      this.loadData
+      this.loadNearbyUsers
     );
   };
 
+  loadNearbyUsers = async () => {
+    this.setState({
+      nearbyUsers: await fetchNearby()
+    })
+  }
+
   componentDidMount() {
-    this.loadData();
+    this.loadNearbyUsers();
+  }
+
+  handleAddFriend= async (user) => {
+    const id = sha256(sessionStorage.getItem('id'), user.id);
+    const content = {
+      type: 'request',
+      from: sessionStorage.getItem('id'),
+      to: user.id,
+      message: 'Hey want to be friends?',
+    }
+    const response = await sendFriendRequest(id, content, {}, {});
+    if(response === true) {
+      alert('friend request sent');
+    } else {
+      alert('error sending friend request');
+    }
   }
 
   render() {
@@ -52,31 +67,31 @@ class Nearby extends Component {
       </div>
 
       <div className="row">
-      {this.state.data.map(data => (
-            <div className="col-md-4 animated fadeIn" key={data.id.value}>
+      {this.state.nearbyUsers.map(user => (
+            <div className="col-md-4 animated fadeIn" key={user.id}>
               <div className="card">
                 <div className="card-body">
                   <div className="avatar">
                     <img
-                      src={data.picture.large}
+                      src={user.imgUrl}
                       className="card-img-top"
                       alt=""
                     />
                   </div>
                   <h5 className="card-title">
-                    {this.uppercase(data.name.first) +
+                    {this.uppercase(user.firstName) +
                       " " +
-                      this.uppercase(data.name.last)}
+                      this.uppercase(user.lastName)}
                   </h5>
                   <p className="card-text">
-                    {data.location.city +
+                    {user.location.city +
                       ", " +
-                      this.uppercase(data.location.state)}
+                      this.uppercase(user.location.state)}
                     <br />
-                    <span className="phone">{data.phone}</span>
+                    <span className="phone">{user.phone}</span>
                   </p>
                 </div>
-                <button className= "friend">Add Friend</button>
+                <button className= "friend" onClick={event => {this.handleAddFriend(user)}}>Add Friend</button>
               </div>
             </div>
           ))}
